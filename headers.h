10ab727bfd293308140b8c2c5f0f5763e9010fbc
cutelittlefish <90677074+littlecutefish@users.noreply.github.com>
2022-11-25 14:38:29 +0800

    1125 upload new hw2

---
+++ headers.h
@@ -0,0 +1,20 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+// OpenGL and FreeGlut headers.
+#include <GLUT/GLUT.h>
+
+// GLM.
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// C++ STL headers.
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+
+#endif
--- trianglemesh.cpp
+++ trianglemesh.cpp
@@ -1,10 +1,4 @@
 #include "trianglemesh.h"
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <cstdlib>
-
-void splitStr2Vec(std::string s, std::vector<std::string>& buf);
 
 // Constructor of a triangle mesh.
 TriangleMesh::TriangleMesh()
@@ -13,46 +7,63 @@ TriangleMesh::TriangleMesh()
     numTriangles = 0;
     objCenter = glm::vec3(0.0f, 0.0f, 0.0f);
     objExtent = glm::vec3(0.0f, 0.0f, 0.0f);
-    // ---------------------------------------------------------------------------
-    // Feel free to add codes.
-    // ---------------------------------------------------------------------------
 }
 
 // Destructor of a triangle mesh.
 TriangleMesh::~TriangleMesh()
 {
-    vertices.clear();
-    vertexIndices.clear();
-    // ---------------------------------------------------------------------------
-    // Feel free to add codes.
-    // ---------------------------------------------------------------------------
+	// -------------------------------------------------------
+	// Add your release code here.
+	// -------------------------------------------------------
 }
 
 // Load the geometry and material data from an OBJ file.
 bool TriangleMesh::LoadFromFile(const std::string& filePath, const bool normalized)
-{
-    // Parse the OBJ file.
-    std::fstream file;
+{	
+	// Parse the OBJ file.
+	// ---------------------------------------------------------------------------
+    // Add your implementation here (HW1 + read *.MTL).
+    // ---------------------------------------------------------------------------
+    std::fstream file,mtl;
+    std::string tmp;
     file.open(filePath, std::ios::in);
     if (!file)
     {
         std::cout << "Open file error!" << std::endl;
     }
     
-    int p = -1, n = -1, t = -1, f = -1;
+    int p = -1, n = -1, t = -1, f = 0, use = -1;
 
     std::vector<glm::vec3> pos2;
     std::vector<glm::vec3> nor2;
     std::vector<glm::vec2> tex2;
     std::vector<std::vector<int>> face2;
-    glm::vec3 center = glm::vec3(0.0f, 0.0f, 0.0f);
+    std::vector<std::string> color2;
     
     glm::vec2 X_maxmin = glm::vec2(0.0f, 0.0f);
     glm::vec2 Y_maxmin = glm::vec2(0.0f, 0.0f);
     glm::vec2 Z_maxmin = glm::vec2(0.0f, 0.0f);
     
+    // 存mtl的變數
+    std::string mtl_temp, color, mtl_value;
+    float ns = 0.0;
+    glm::vec3 ka = glm::vec3(0.0f,0.0f,0.0f);
+    glm::vec3 kd = glm::vec3(0.0f,0.0f,0.0f);
+    glm::vec3 ks = glm::vec3(0.0f,0.0f,0.0f);
+    std::vector<glm::vec3> ka2;
+    std::vector<glm::vec3> kd2;
+    std::vector<glm::vec3> ks2;
+    std::vector<float> ns2;
+    std::vector<std::string> mtlName;
+//    int num=0, newnum=0;
+    
+    std::vector<unsigned int> vertexindex;
+    std::vector<std::vector<unsigned int>> vertexindex2;
+    
+//    SubMesh submesh;
+    
     if (file.is_open()) {
-        std::string line;
+        std::string line, mtlPath;
 
         
         while (getline(file, line)) {
@@ -60,22 +71,66 @@ bool TriangleMesh::LoadFromFile(const std::string& filePath, const bool normaliz
             std::string type;
             ss >> type;
             
-            std::string find_space = " ";
+            if(type == "mtllib"){  // 讀入mtl檔
+                ss >> mtlPath;  // mtl 的檔案路徑
+                
+                for(int i = 0; i < mtlPath.length(); i++){
+                    if(mtlPath[i] == '.') tmp = mtlPath.substr(0,i);
+                }
+                mtlPath = "/Users/liuliyu/Documents/Junior/Computer_graphic/HW2/ICG2022_HW2/ICG2022_HW2/models/"+ tmp + "/" + mtlPath;
+                
+                int flag_mtl = 0; //表示未讀過mtl檔案
+                if(!flag_mtl){
+                    mtl.open(mtlPath, std::ios::in);
+                    if(!mtl){
+                        std::cout << "Open mtl error!" << std::endl;
+                    }
+                    
+                    flag_mtl = 1; //表示已經讀過mtl檔案了
+                    
+                    while (getline(mtl, line)) {
+                        std::stringstream stemp(line);
+                        stemp >> mtl_temp;
+                        if (mtl_temp == "newmtl"){ // 存顏色
+                            stemp >> color;
+                            mtlName.push_back(color);
+                        }
+                        else if (mtl_temp == "Ns"){
+                            stemp >> ns;
+                            ns2.push_back(ns);
+                        }
+                        else if (mtl_temp == "Ka"){
+                            stemp >> ka[0] >> ka[1] >>ka[2];
+                            ka2.push_back(ka);
+                        }
+                        else if (mtl_temp == "Kd"){
+                            stemp >> kd[0] >> kd[1] >>kd[2];
+                            kd2.push_back(kd);
+                        }
+                        else if (mtl_temp == "Ks"){
+                            stemp >> ks[0] >> ks[1] >>ks[2];
+                            ks2.push_back(ks);
+                            mtl_temp = " "; //還原初始化
+                        }
+                    }
+                    mtl.close();
+                }
+            }
             
             // position
             if (type == "v") {
                 glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f);
                 ss >> pos.x >> pos.y >> pos.z;
                 pos2.push_back(pos);
-                center.x += pos.x;
-                center.y += pos.y;
-                center.z += pos.z;
+                objCenter.x += pos.x;
+                objCenter.y += pos.y;
+                objCenter.z += pos.z;
                 if(pos.x > X_maxmin.x) X_maxmin.x = pos.x; //x max
                 else if(pos.x < X_maxmin.y) X_maxmin.y = pos.x; //x min
                 
                 if(pos.y > Y_maxmin.x) Y_maxmin.x = pos.y; //y max
                 else if(pos.y < Y_maxmin.y) Y_maxmin.y = pos.y; //y min
-                 
+                
                 if(pos.z > Z_maxmin.x) Z_maxmin.x = pos.z; //z max
                 else if(pos.z < Z_maxmin.y) Z_maxmin.y = pos.z; //z min
                 p++;
@@ -94,41 +149,50 @@ bool TriangleMesh::LoadFromFile(const std::string& filePath, const bool normaliz
                 tex2.push_back(tex);
                 t++;
             }
-            else if (type == "f") {
-                f++;
+            
+            else if (type == "usemtl"){
+                use++;
+                ss >> color;
+                color2.push_back(color);
+
+                if(vertexindex.size() != 0){
+                    vertexindex2.push_back(vertexindex);
+                    vertexindex.clear();
+                }
             }
             
             std::vector<int> face(20), temp;
             while(ss >> face[0]) {
-                if (type == "f") {
-                    int k = 1;
-                    char c;
-                    for(int j = 0; j < 2; j++){
-                        ss >> c >> face[k];
-                        k++;
-                    }
-                    
-                    for(int j = 0; j < 3; j++){
-                        face[j] = abs(face[j]);
-                    }
-                    temp.push_back(face[0] - 1);
-                    face2.push_back(face);
+                if(type == "f") f++;
+                int k = 1;
+                char c;
+                for(int j = 0; j < 2; j++){
+                    ss >> c >> face[k];
+                    k++;
                 }
+                
+                for(int j = 0; j < 3; j++){
+                    face[j] = abs(face[j]);
+                }
+                temp.push_back(face[0] - 1);
+                face2.push_back(face);
+                
                 if(temp.size() == 3){
                     for(int count = 0; count < 3; count++){
-                        vertexIndices.push_back(temp[count]);
+                        vertexindex.push_back(temp[count]);
                     }
                 }
                 else if (temp.size() > 3){
                     for(int count = 2; count < temp.size(); count++){
-                        vertexIndices.push_back(temp[0]);
-                        vertexIndices.push_back(temp[count - 1]);
-                        vertexIndices.push_back(temp[count]);
+                        vertexindex.push_back(temp[0]);
+                        vertexindex.push_back(temp[count - 1]);
+                        vertexindex.push_back(temp[count]);
                     }
                 }
             }
-            
         }
+        vertexindex2.push_back(vertexindex);
+        
         VertexPTN vertexptn;
 
         for (int i = 0; i < pos2.size(); i++){
@@ -139,17 +203,53 @@ bool TriangleMesh::LoadFromFile(const std::string& filePath, const bool normaliz
         }
         numVertices = p + 1;
         
+        for (int i = 0; i < nor2.size(); i++){
+            vertexptn.normal.x = nor2[i][0];
+            vertexptn.normal.y = nor2[i][1];
+            vertexptn.normal.z = nor2[i][2];
+            vertices.push_back(vertexptn);
+        }
+        
+        for (int i = 0; i < tex2.size(); i++){
+            vertexptn.texcoord.x = tex2[i][0];
+            vertexptn.texcoord.y = tex2[i][1];
+            vertices.push_back(vertexptn);
+        }
+        
         file.close();
     }
     
-    center.x /= p;
-    center.y /= p;
-    center.z /= p;
+    SubMesh submesh;
+    PhongMaterial* phong[100];  //宣告一個指標p (動態配置記憶體) 之後要delete p
+    for(int i = 0; i < use + 1; i++){
+        phong[i] = new PhongMaterial();
+        for(int j = 0; j < mtlName.size(); j++){
+            if(color2[i] == mtlName[j]){
+                phong[i]->SetNs(ns2[j]);
+                phong[i]->SetKs(ks2[j]);
+                phong[i]->SetKd(kd2[j]);
+                phong[i]->SetKa(ka2[j]);
+                phong[i]->SetName(mtlName[j]);
+            }
+        }
+        submesh.material=phong[i];
+        submesh.vertexIndices = vertexindex2[i];
+        subMeshes.push_back(submesh);
+    }
+
+    objCenter.x /= p;
+    objCenter.y /= p;
+    objCenter.z /= p;
     
-    // Normalize the geometry data.
-    if (normalized) {
-        // -----------------------------------------------------------------------
-        // Add your implementation.
+    for(int i = 0; i < subMeshes.size(); i++){
+        numTriangles += (subMeshes[i].vertexIndices.size() / 3);
+    }
+    
+	// Normalize the geometry data.
+	if (normalized) {
+		// -----------------------------------------------------------------------
+		// Add your normalization code here (HW1).
+		// -----------------------------------------------------------------------
         float max = X_maxmin.x - X_maxmin.y;
         if(max < Y_maxmin.x - Y_maxmin.y) max = Y_maxmin.x - Y_maxmin.y;
         if(max < Z_maxmin.x - Z_maxmin.y) max = Z_maxmin.x - Z_maxmin.y;
@@ -159,61 +259,25 @@ bool TriangleMesh::LoadFromFile(const std::string& filePath, const bool normaliz
         objExtent.z = (Z_maxmin.x - Z_maxmin.y)/max;
         
         for(int i = 0; i < numVertices; i++){
-            vertices[i].position.x = (vertices[i].position.x - center.x)/max;
-            vertices[i].position.y = (vertices[i].position.y - center.y)/max;
-            vertices[i].position.z = (vertices[i].position.z - center.z)/max;
+            vertices[i].position.x = (vertices[i].position.x - objCenter.x)/max;
+            vertices[i].position.y = (vertices[i].position.y - objCenter.y)/max;
+            vertices[i].position.z = (vertices[i].position.z - objCenter.z)/max;
         }
-        
-        // -----------------------------------------------------------------------
-    }
-    return true;
-}
-
-// Apply transform to vertices using GPU.
-void TriangleMesh::ApplyTransformCPU(const glm::mat4x4& mvpMatrix)
-{
-    for (int i = 0 ; i < numVertices; ++i) {
-        glm::vec4 p = mvpMatrix * glm::vec4(vertices[i].position, 1.0f);
-        if (p.w != 0.0f) {
-            float inv = 1.0f / p.w;
-            vertices[i].position.x = p.x * inv;
-            vertices[i].position.y = p.y * inv;
-            vertices[i].position.z = p.z * inv;
-        }
-    }
+	}
+	return true;
 }
 
 // Show model information.
 void TriangleMesh::ShowInfo()
 {
-    numTriangles = (int)vertexIndices.size() / 3;
-    std::cout << "# Vertices: " << numVertices << std::endl;
-    std::cout << "# Triangles: " << numTriangles << std::endl;
-    std::cout << "Model Center: " << objCenter.x << ", " << objCenter.y << ", " << objCenter.z << std::endl;
-    std::cout << "Model Extent: " << objExtent.x << " x " << objExtent.y << " x " << objExtent.z << std::endl;
-}
-
-
-
-void TriangleMesh::CreateVertexBuffers(GLuint &vbo)
-{
-    // Generate the vertex buffer for cube.
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(VertexPTN) * numVertices, &(vertices[0]), GL_STATIC_DRAW);
-}
-
-void TriangleMesh::CreateIndexBuffers(GLuint &ibo){
-    // Generate the index buffer.
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * numTriangles * 3, &(vertexIndices[0]), GL_STATIC_DRAW);
-}
-
-void TriangleMesh::RenderOBJ(GLuint &vbo, GLuint &ibo){
-    glColor3f(1.0f, 1.0f, 1.0f);    // Not good, only used for quick demo.
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexPTN), 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glDrawElements(GL_TRIANGLES, numTriangles * 3, GL_UNSIGNED_INT, 0);
+	std::cout << "# Vertices: " << numVertices << std::endl;
+	std::cout << "# Triangles: " << numTriangles << std::endl;
+	std::cout << "Total " << subMeshes.size() << " subMeshes loaded" << std::endl;
+	for (unsigned int i = 0; i < subMeshes.size(); ++i) {
+		const SubMesh& g = subMeshes[i];
+		std::cout << "SubMesh " << i << " with material: " << g.material->GetName() << std::endl;
+		std::cout << "Num. triangles in the subMesh: " << g.vertexIndices.size() / 3 << std::endl;
+	}
+	std::cout << "Model Center: " << objCenter.x << ", " << objCenter.y << ", " << objCenter.z << std::endl;
+	std::cout << "Model Extent: " << objExtent.x << " x " << objExtent.y << " x " << objExtent.z << std::endl;
 }
